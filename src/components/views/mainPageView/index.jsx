b8030d6897d16c12e3e678d2e7f7da9977cee98c
hoolek77 <szymon.kin@gmail.com>
2021-03-19 13:24:56 +0100

    created reusable table component

--- src/components/views/mainPageView/index.jsx
+++ src/components/views/mainPageView/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import SearchBar from './SearchBar'
 import { useSearch } from '../../../hooks/useSearch'
+import EnhancedTable, { rows, headCells } from '../../Table'
 
 export const MainPageView = () => {
   const [hotels, search] = useSearch()
@@ -8,6 +9,18 @@ export const MainPageView = () => {
   return (
     <div id="MainPageView">
       <SearchBar onSearchSubmit={search} />
+      <div
+        style={{
+          width: '50%',
+        }}
+      >
+        <EnhancedTable
+          title="users"
+          rows={rows}
+          headCells={headCells}
+          rowsPerPageCount="10"
+        />
+      </div>
     </div>
   )
 }
